9cf660cffa34a90b21b8b71152e027847a76b3ef
Daryl Fung <darylfung96@gmail.com>
2017-12-27 19:46:12 +0800

    added attention decoder

--- decoder.py
+++ decoder.py
@@ -18,3 +18,27 @@
 
 
 """
+
+def prenet(inputs):
+    with tf.variable_scope("prenet"):
+        layer1 = tf.nn.dropout(tf.layers.dense(inputs, 256, activation=tf.nn.relu), keep_prob=0.5)
+        layer2 = tf.nn.dropout(tf.layers.dense(layer1, 128, activation=tf.nn.relu), keep_prob=0.5)
+    return layer2
+
+
+def attention_decoder(inputs, memory, num_units):
+
+    decoder_cell = tf.contrib.rnn.GRUCell(num_units)
+
+    attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(num_units, memory, normalize=True,
+        probability_fn=tf.nn.softmax)  # probability_fn uses Softmax function to get the weights to focus on the memory
+    dec_cells_and_attentions = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism, num_units)
+
+    outputs, _ = tf.nn.dynamic_cell(dec_cells_and_attentions, inputs, dtype=tf.float32)
+
+    return outputs
+
+
+def decoder(inputs):
+    outputs = prenet(inputs)
+
