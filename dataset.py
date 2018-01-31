c4ed03b47799f22ac5c24a7abae7e6fc5ef98b92
Daryl Fung <darylfung96@gmail.com>
2018-01-30 23:39:54 -0600

    finish dumping preprocessed files

--- dataset.py
+++ dataset.py
@@ -13,6 +13,8 @@ class LJDataset(Dataset):
     def __init__(self, folder_dir):
         super(LJDataset, self).__init__(folder_dir=folder_dir)
 
+
+    #TODO: create threads for preprocessing the data to speedup the process
     def run(self):
         if not os.path.isdir(self._folder_dir):
             raise FileNotFoundError
@@ -21,23 +23,29 @@ class LJDataset(Dataset):
 
         file_results = []
 
+        if not os.path.isdir('training'):
+            os.mkdir('training')
+
         with open(csv_file_path) as f:
             csv_data = f.read()
             csv_data = csv_data.split('\n')
 
             for data in tqdm(csv_data):
                 linear_output, mel_output, filename, text = self._extract_data(data)  # it was always outputting 1 in ever rows and columns, figured out the problem was with min_level_db which should be -100 but was 100.
-                linear_file = 'training/'+filename+'-linear'
-                mel_file = 'training/'+filename+'-mel'
-                pickle.dump(linear_output, linear_file)
-                pickle.dump(mel_output, mel_file)
+                linear_file = 'training/'+filename+'-linear.pkl'
+                mel_file    = 'training/'+filename+'-mel.pkl'
+
+                with open(linear_file, 'wb') as f:
+                    pickle.dump(linear_output, f)
+                with open(mel_file, 'wb') as f:
+                    pickle.dump(mel_output, f)
 
                 result = linear_file + '|' + mel_file + '|' + text
                 file_results.append(result)
 
 
         with open('training/train.txt', 'r') as f:
-            for result in file_results:
+            for result in tqdm(file_results):
                 f.write(result)
 
 
@@ -52,5 +60,5 @@ class LJDataset(Dataset):
         return linear_output, mel_output, data[0], data[1]  # linear output, mel output, text
 
 
-dataset = LJDataset("LJSpeech-1.0")
+dataset = LJDataset("../keith_tacotron/tacotron/LJSpeech-1.0")
 dataset.run()
