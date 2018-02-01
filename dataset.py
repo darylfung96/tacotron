972b86789487031a283b98bb2032269576582135
Daryl Fung <darylfung96@gmail.com>
2018-02-01 07:25:38 -0600

    added multithread

--- dataset.py
+++ dataset.py
@@ -1,6 +1,8 @@
 import pickle
 import os
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
 
 from preprocess import process_wav
 
@@ -30,18 +32,15 @@ class LJDataset(Dataset):
             csv_data = f.read()
             csv_data = csv_data.split('\n')
 
+            processor = ProcessPoolExecutor(max_workers=multiprocessing.cpu_count())
+            file_results = []
+
             for data in tqdm(csv_data):
-                linear_output, mel_output, filename, text = self._extract_data(data)  # it was always outputting 1 in ever rows and columns, figured out the problem was with min_level_db which should be -100 but was 100.
-                linear_file = 'training/'+filename+'-linear.pkl'
-                mel_file    = 'training/'+filename+'-mel.pkl'
+                linear_output, mel_output, text = self._extract_data(data)  # it was always outputting 1 in ever rows and columns, figured out the problem was with min_level_db which should be -100 but was 100.
 
-                with open(linear_file, 'wb') as f:
-                    pickle.dump(linear_output, f)
-                with open(mel_file, 'wb') as f:
-                    pickle.dump(mel_output, f)
-
-                result = linear_file + '|' + mel_file + '|' + text
+                result = processor.submit(self._extract_data, data)
                 file_results.append(result)
+
 
 
         with open('training/train.txt', 'r') as f:
@@ -53,11 +52,22 @@ class LJDataset(Dataset):
         data = data.split('|')  # data[0] = filename, data[1] = text
 
         wav_file = data[0] + '.wav'
+        text = data[1]
 
         wav_path = os.path.join(self._folder_dir, 'wavs', wav_file)
         linear_output, mel_output = process_wav(wav_path)
 
-        return linear_output, mel_output, data[0], data[1]  # linear output, mel output, text
+        linear_file = 'training/' + wav_file + '-linear.pkl'
+        mel_file = 'training/' + wav_file + '-mel.pkl'
+
+        with open(linear_file, 'wb') as f:
+            pickle.dump(linear_output, f)
+        with open(mel_file, 'wb') as f:
+            pickle.dump(mel_output, f)
+
+        result = linear_file + '|' + mel_file + '|' + text
+
+        return result  # linear output, mel output, text
 
 
 dataset = LJDataset("../keith_tacotron/tacotron/LJSpeech-1.0")
